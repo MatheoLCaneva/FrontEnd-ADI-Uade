5f0cae5d4b89222e0fb404cbc0a58fce50bfccba
Nicolás Facón <nicolasfacon@malffdigital.com.ar>
2023-07-09 14:35:18 -0300

    Indent UNavigator.jsx

--- android/app/navigation/UNavigator.jsx
+++ android/app/navigation/UNavigator.jsx
@@ -1,48 +1,70 @@
-import React from "react";
-import{ AppRegistry, ScrollView, Image, Text, View } from 'react-native';
-import UserHome from "../ui/screens/user/UHome";
-import UFilters from "../ui/screens/user/UFilters";
-import NavigatorConstant from "./NavigatorConstant";
-import UserNavigator from "./UserNavigator";
+import React from 'react';
+import { AppRegistry, ScrollView, Image, Text, View } from 'react-native';
+import UserHome from '../ui/screens/user/UHome';
+import UFilters from '../ui/screens/user/UFilters';
+import NavigatorConstant from './NavigatorConstant';
+import UserNavigator from './UserNavigator';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import UserProfile from "../ui/screens/user/UProfile";
-import BookingsNavigator from "./BookingsNavigator";
+import UserProfile from '../ui/screens/user/UProfile';
+import BookingsNavigator from './BookingsNavigator';
 
 const Tab = createBottomTabNavigator();
 
 export default function UserTabsNavigator(props) {
-  
-    return (
-        <Tab.Navigator initialRouteName={NavigatorConstant.NAVIGATOR.UsersTabsNavigator} screenOptions={{ headerShown: true , tabBarStyle:{backgroundColor:'#E01D6F'}}} >
-            <Tab.Screen name={NavigatorConstant.USER_TABS.HOME} component={UserNavigator} options={{ headerShown: false,
+  return (
+    <Tab.Navigator
+      initialRouteName={NavigatorConstant.NAVIGATOR.UsersTabsNavigator}
+      screenOptions={{
+        headerShown: true,
+        tabBarStyle: { backgroundColor: '#E01D6F' },
+      }}>
+      <Tab.Screen
+        name={NavigatorConstant.USER_TABS.HOME}
+        component={UserNavigator}
+        options={{
+          headerShown: false,
           tabBarLabel: '',
-          tabBarIcon: ({}) => (
-            <Image
-            source={require('../assets/icons/home.png')}
-            
-            size={26}
-            /> ), }} />
-            <Tab.Screen name={NavigatorConstant.USER_TABS.FILTERS} component={UFilters} options={{ headerStyle: { backgroundColor: '#E01D6F' }, headerTintColor: '#fff', headerTitleStyle: { fontWeight: 'normal' }, headerTitleAlign: 'center' ,
+          tabBarIcon: ({ }) => (
+            <Image source={require('../assets/icons/home.png')} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={NavigatorConstant.USER_TABS.FILTERS}
+        component={UFilters}
+        options={{
+          headerStyle: { backgroundColor: '#E01D6F' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontWeight: 'normal' },
+          headerTitleAlign: 'center',
           tabBarLabel: '',
-          tabBarIcon: ({}) => (
-            <Image
-            source={require('../assets/icons/search.png')}
-            size={26}
-            /> ), }} />
-            <Tab.Screen name={NavigatorConstant.USER_TABS.BOOKINGS} component={BookingsNavigator} options={{ headerShown: false ,
+          tabBarIcon: ({ }) => (
+            <Image source={require('../assets/icons/search.png')} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={NavigatorConstant.USER_TABS.BOOKINGS}
+        component={BookingsNavigator}
+        options={{
+          headerShown: false,
           tabBarLabel: '',
-          tabBarIcon: ({}) => (
-            <Image
-            source={require('../assets/icons/bookmark.png')}
-            size={26}
-            /> ), }} />
-            <Tab.Screen name={NavigatorConstant.USER_TABS.PROFILE} component={UserProfile} options={{ headerShown: false ,
+          tabBarIcon: ({ }) => (
+            <Image source={require('../assets/icons/bookmark.png')} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={NavigatorConstant.USER_TABS.PROFILE}
+        component={UserProfile}
+        options={{
+          headerShown: false,
           tabBarLabel: '',
-          tabBarIcon: ({}) => (
-            <Image
-            source={require('../assets/icons/settings.png')}
-            size={26}
-            /> ), }} />
-        </Tab.Navigator>
-    )
+          tabBarIcon: ({ }) => (
+            <Image source={require('../assets/icons/settings.png')} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
